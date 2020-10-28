793a5e3905aa90295b190adb5ed06609d926c9be
Ubuntu <ubuntu@ip-172-31-0-35.us-west-2.compute.internal>
2020-10-28 13:14:42 +0000

    Finished api call for characters, and character detail

---
+++ src/components/app/services/avatar-api.js
@@ -0,0 +1,10 @@
+export const getCharacters = () => {
+  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+    .then(res => res.json())
+    .catch(err => console.log('error', err));
+};
+
+export const getDetails = (characterId) => {
+  return fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters/${characterId}`)
+    .then(res => res.json());
+}
